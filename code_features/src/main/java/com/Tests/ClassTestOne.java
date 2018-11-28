b8a0c4b80f73d91ca7fe662d6ae7f58d07df6c05
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2018-11-28 12:08:57 +0300

    commit_2

--- code_features/src/main/java/com/Tests/ClassTestOne.java
+++ code_features/src/main/java/com/Tests/ClassTestOne.java
@@ -5,9 +5,16 @@ public class ClassTestOne {
 
     public void test_one() {
 //        Assert.assertTrue("", true);
+        Integer g = 10; //ㅇㅇㅇ
     }
 
     public void  test_two() {
 //        Assert.assertFalse("Fasle", false);
+    }
+
+    public void foo (){
+        for (int i = 0; i < 12; i++) {
+
+        }
     }
 }
