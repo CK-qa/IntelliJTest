f8c0177cd0863edac9e33f3f07c0048b1fc3b394
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2018-11-30 16:43:02 +0300

    Merge remote-tracking branch 'remotes/additional/master'
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/modules.xml

--- src/com/company/LongClassNameForSomeTestNeedsAndBugVerifying.java
+++ src/com/company/LongClassNameForSomeTestNeedsAndBugVerifying.java
@@ -5,12 +5,4 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.containsString;
 
-public class LongClassNameForSomeTestNeedsAndBugVerifying {
 
-
-    @Test
-    public void jUnitTestNegative() {
-        System.out.println("jUnitTestNegative running");
-        Assert.assertThat("Not ok", "String", containsString("String1"));
-    }
-}
