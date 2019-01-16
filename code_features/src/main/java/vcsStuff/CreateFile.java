d1e9ea70279a7c051499afee40370aed1be5c8ae
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2019-01-16 20:33:45 +0300

    build.gradle added

---
+++ code_features/src/main/java/vcsStuff/CreateFile.java
@@ -0,0 +1,18 @@
+package vcsStuff;
+
+import java.io.*;
+
+public class CreateFile {
+
+	public static void main(String[] args) throws IOException {
+
+		String path = "C:\\Users\\viktoria.bozhko\\IdeaProjects\\IntelliJTest\\code_features\\src\\main\\java\\vcsStuff\\generated\\";
+
+		File file;
+		for (int i = 0; i < 3; i++) {
+			file = new File(path + "filetest" + i + ".txt");
+			file.createNewFile();
+		}
+	}
+}
+
