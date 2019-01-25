99e3df4c617d8267c3b9157907ec3a4be65b7885
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2019-01-25 13:30:33 +0300

    removed files with strange chars

---
+++ code_features/src/main/java/vcsStuff/package_with_longname/package_with_longname_2/package_with_longname_3/package_with_longname_4/package_with_longname_5/package_with_longname_6/package_with_longname_7/C.java
@@ -0,0 +1,4 @@
+package vcsStuff.package_with_longname.package_with_longname_2.package_with_longname_3.package_with_longname_4.package_with_longname_5.package_with_longname_6.package_with_longname_7;
+
+public class C {
+}
