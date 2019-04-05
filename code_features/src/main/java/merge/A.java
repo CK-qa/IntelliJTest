1d9c5bee91644df38292a2b386dc40ed13ebe257
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2019-04-05 16:13:30 +0300

    A - win

--- code_features/src/main/java/merge/A.java
+++ code_features/src/main/java/merge/A.java
@@ -3,6 +3,8 @@ package merge;
 public class A {
 
 	public static void main(String[] args) {
-		System.out.println("peewpew");
+		System.out.println("peewpew" + s);
 	}
+
+	static String s = "asa";
 }
