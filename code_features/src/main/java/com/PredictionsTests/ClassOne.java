3c49de022a29d7b55114262e47163c02f0ea0089
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2019-04-30 13:19:56 +0300

    one two

--- code_features/src/main/java/com/PredictionsTests/ClassOne.java
+++ code_features/src/main/java/com/PredictionsTests/ClassOne.java
@@ -2,10 +2,18 @@ package com.PredictionsTests;
 
 public class ClassOne {
 
+	@Override
+	public String toString() {
+		return new org.apache.commons.lang3.builder.ToStringBuilder(this)
+				.toString();
+	}
+
 	/**
 	 * @return "sss"
 	 */
 	public String foo() {
 		return "Sss";
 	}
+
+
 }
--- code_features/src/main/java/com/PredictionsTests/ClassTwo.java
+++ code_features/src/main/java/com/PredictionsTests/ClassTwo.java
@@ -2,6 +2,9 @@ package com.PredictionsTests;
 
 public class ClassTwo {
 
+	public ClassTwo() {
+	}
+
 	public int bar(int a) {
 		return a;
 	}
