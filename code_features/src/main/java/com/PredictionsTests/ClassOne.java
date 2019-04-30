c34e8dbef8200fe69b6eb03af74e3ae1ebbed0a5
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2019-04-30 13:28:10 +0300

    one two

--- code_features/src/main/java/com/PredictionsTests/ClassOne.java
+++ code_features/src/main/java/com/PredictionsTests/ClassOne.java
@@ -7,6 +7,248 @@ import java.util.stream.IntStream;
 
 public class ClassOne {
 
+	String a;
+	Integer i;
+
+	public static String toString(int i, int radix) {
+		return Integer.toString(i, radix);
+	}
+
+	public static String toUnsignedString(int i, int radix) {
+		return Integer.toUnsignedString(i, radix);
+	}
+
+	public static String toHexString(int i) {
+		return Integer.toHexString(i);
+	}
+
+	public static String toOctalString(int i) {
+		return Integer.toOctalString(i);
+	}
+
+	public static String toBinaryString(int i) {
+		return Integer.toBinaryString(i);
+	}
+
+	public static String toString(int i) {
+		return Integer.toString(i);
+	}
+
+	public static String toUnsignedString(int i) {
+		return Integer.toUnsignedString(i);
+	}
+
+	public static int parseInt(String s, int radix) throws NumberFormatException {
+		return Integer.parseInt(s, radix);
+	}
+
+	public static int parseInt(String s) throws NumberFormatException {
+		return Integer.parseInt(s);
+	}
+
+	public static int parseUnsignedInt(String s, int radix) throws NumberFormatException {
+		return Integer.parseUnsignedInt(s, radix);
+	}
+
+	public static int parseUnsignedInt(String s) throws NumberFormatException {
+		return Integer.parseUnsignedInt(s);
+	}
+
+	public static Integer valueOf(String s, int radix) throws NumberFormatException {
+		return Integer.valueOf(s, radix);
+	}
+
+	public static Integer valueOf(String s) throws NumberFormatException {
+		return Integer.valueOf(s);
+	}
+
+	public byte byteValue() {
+		return i.byteValue();
+	}
+
+	public short shortValue() {
+		return i.shortValue();
+	}
+
+	public int intValue() {
+		return i.intValue();
+	}
+
+	public long longValue() {
+		return i.longValue();
+	}
+
+	public float floatValue() {
+		return i.floatValue();
+	}
+
+	public double doubleValue() {
+		return i.doubleValue();
+	}
+
+	public static int hashCode(int value) {
+		return Integer.hashCode(value);
+	}
+
+	public static Integer getInteger(String nm) {
+		return Integer.getInteger(nm);
+	}
+
+	public static Integer getInteger(String nm, int val) {
+		return Integer.getInteger(nm, val);
+	}
+
+	public static Integer getInteger(String nm, Integer val) {
+		return Integer.getInteger(nm, val);
+	}
+
+	public static Integer decode(String nm) throws NumberFormatException {
+		return Integer.decode(nm);
+	}
+
+	public int compareTo(Integer anotherInteger) {
+		return i.compareTo(anotherInteger);
+	}
+
+	public static int compare(int x, int y) {
+		return Integer.compare(x, y);
+	}
+
+	public static int compareUnsigned(int x, int y) {
+		return Integer.compareUnsigned(x, y);
+	}
+
+	public static long toUnsignedLong(int x) {
+		return Integer.toUnsignedLong(x);
+	}
+
+	public static int divideUnsigned(int dividend, int divisor) {
+		return Integer.divideUnsigned(dividend, divisor);
+	}
+
+	public static int remainderUnsigned(int dividend, int divisor) {
+		return Integer.remainderUnsigned(dividend, divisor);
+	}
+
+	public static int highestOneBit(int i) {
+		return Integer.highestOneBit(i);
+	}
+
+	public static int lowestOneBit(int i) {
+		return Integer.lowestOneBit(i);
+	}
+
+	public static int numberOfLeadingZeros(int i) {
+		return Integer.numberOfLeadingZeros(i);
+	}
+
+	public static int numberOfTrailingZeros(int i) {
+		return Integer.numberOfTrailingZeros(i);
+	}
+
+	public static int bitCount(int i) {
+		return Integer.bitCount(i);
+	}
+
+	public static int rotateLeft(int i, int distance) {
+		return Integer.rotateLeft(i, distance);
+	}
+
+	public static int rotateRight(int i, int distance) {
+		return Integer.rotateRight(i, distance);
+	}
+
+	public static int reverse(int i) {
+		return Integer.reverse(i);
+	}
+
+	public static int signum(int i) {
+		return Integer.signum(i);
+	}
+
+	public static int reverseBytes(int i) {
+		return Integer.reverseBytes(i);
+	}
+
+	public static int sum(int a, int b) {
+		return Integer.sum(a, b);
+	}
+
+	public static int max(int a, int b) {
+		return Integer.max(a, b);
+	}
+
+	public static int min(int a, int b) {
+		return Integer.min(a, b);
+	}
+
+	int b;
+
+	public ClassOne(String a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public static String join(CharSequence delimiter, CharSequence... elements) {
+		return String.join(delimiter, elements);
+	}
+
+	public static String join(CharSequence delimiter, Iterable<? extends CharSequence> elements) {
+		return String.join(delimiter, elements);
+	}
+
+	public static String format(String format, Object... args) {
+		return String.format(format, args);
+	}
+
+	public static String format(Locale l, String format, Object... args) {
+		return String.format(l, format, args);
+	}
+
+	public static String valueOf(Object obj) {
+		return String.valueOf(obj);
+	}
+
+	public static String valueOf(char[] data) {
+		return String.valueOf(data);
+	}
+
+	public static String valueOf(char[] data, int offset, int count) {
+		return String.valueOf(data, offset, count);
+	}
+
+	public static String copyValueOf(char[] data, int offset, int count) {
+		return String.copyValueOf(data, offset, count);
+	}
+
+	public static String copyValueOf(char[] data) {
+		return String.copyValueOf(data);
+	}
+
+	public static String valueOf(boolean b) {
+		return String.valueOf(b);
+	}
+
+	public static String valueOf(char c) {
+		return String.valueOf(c);
+	}
+
+	public static String valueOf(int i) {
+		return String.valueOf(i);
+	}
+
+	public static String valueOf(long l) {
+		return String.valueOf(l);
+	}
+
+	public static String valueOf(float f) {
+		return String.valueOf(f);
+	}
+
+	public static String valueOf(double d) {
+		return String.valueOf(d);
+	}
+
 	public int length() {
 		return a.length();
 	}
@@ -176,14 +418,6 @@ public class ClassOne {
 		return a.split(regex);
 	}
 
-	public static String join(CharSequence delimiter, CharSequence... elements) {
-		return String.join(delimiter, elements);
-	}
-
-	public static String join(CharSequence delimiter, Iterable<? extends CharSequence> elements) {
-		return String.join(delimiter, elements);
-	}
-
 	public String toLowerCase(Locale locale) {
 		return a.toLowerCase(locale);
 	}
@@ -208,58 +442,6 @@ public class ClassOne {
 		return a.toCharArray();
 	}
 
-	public static String format(String format, Object... args) {
-		return String.format(format, args);
-	}
-
-	public static String format(Locale l, String format, Object... args) {
-		return String.format(l, format, args);
-	}
-
-	public static String valueOf(Object obj) {
-		return String.valueOf(obj);
-	}
-
-	public static String valueOf(char[] data) {
-		return String.valueOf(data);
-	}
-
-	public static String valueOf(char[] data, int offset, int count) {
-		return String.valueOf(data, offset, count);
-	}
-
-	public static String copyValueOf(char[] data, int offset, int count) {
-		return String.copyValueOf(data, offset, count);
-	}
-
-	public static String copyValueOf(char[] data) {
-		return String.copyValueOf(data);
-	}
-
-	public static String valueOf(boolean b) {
-		return String.valueOf(b);
-	}
-
-	public static String valueOf(char c) {
-		return String.valueOf(c);
-	}
-
-	public static String valueOf(int i) {
-		return String.valueOf(i);
-	}
-
-	public static String valueOf(long l) {
-		return String.valueOf(l);
-	}
-
-	public static String valueOf(float f) {
-		return String.valueOf(f);
-	}
-
-	public static String valueOf(double d) {
-		return String.valueOf(d);
-	}
-
 	public String intern() {
 		return a.intern();
 	}
@@ -270,14 +452,6 @@ public class ClassOne {
 
 	public IntStream codePoints() {
 		return a.codePoints();
-	}
-
-	String a;
-	int b;
-
-	public ClassOne(String a, int b) {
-		this.a = a;
-		this.b = b;
 	}
 
 	@Override
