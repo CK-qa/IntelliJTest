90a11f0e4cc8fb5e26c917d0b03f4eec796d4faa
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2019-04-30 13:24:39 +0300

    two

--- code_features/src/main/java/com/PredictionsTests/ClassTwo.java
+++ code_features/src/main/java/com/PredictionsTests/ClassTwo.java
@@ -3,6 +3,12 @@ package com.PredictionsTests;
 public class ClassTwo {
 
 	float f;
+
+	public ClassTwo(float f, Integer i) {
+		this.f = f;
+		this.i = i;
+	}
+
 	Integer i;
 
 	public ClassTwo() {
