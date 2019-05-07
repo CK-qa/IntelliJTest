657e27e644dcbf5994e880a96b91883889aa2a39
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2019-05-07 12:42:34 +0300

    classA--

--- code_features/src/main/java/com/bugfix/a_branch_aa/a_long_package_name/another_long_package_name/one_more_another_long_package_name/and_one_more_another_long_package_name/and_even_one_more_another_long_package_name/ClassB.java
+++ code_features/src/main/java/com/bugfix/a_branch_aa/a_long_package_name/another_long_package_name/one_more_another_long_package_name/and_one_more_another_long_package_name/and_even_one_more_another_long_package_name/ClassB.java
@@ -1,4 +1,6 @@
 package com.bugfix.a_branch_aa.a_long_package_name.another_long_package_name.one_more_another_long_package_name.and_one_more_another_long_package_name.and_even_one_more_another_long_package_name;
 
 public class ClassB {
+
+	//test
 }
--- code_features/src/main/java/com/ignores/ClassA.java
+++
@@ -1,4 +0,0 @@
-package com.ignores;
-
-public class ClassA {
-}
