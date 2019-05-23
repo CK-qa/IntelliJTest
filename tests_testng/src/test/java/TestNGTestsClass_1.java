6ea8445aad56d3062a107d4fdc96b71c9179f91d
Viktoria Bozhko <victoria.bozhko@gmail.com>
2019-05-23 13:19:06 +0300

    commit and push

--- tests_testng/src/test/java/TestNGTestsClass_1.java
+++ tests_testng/src/test/java/TestNGTestsClass_1.java
@@ -3,71 +3,68 @@ import org.testng.annotations.*;
 
 public class TestNGTestsClass_1 {
 
-    @BeforeClass
-    void beforeClass() {
-        System.out.println("Before Class");
-    }
+	@BeforeClass
+	void beforeClass() {
+		System.out.println("Before Class");
+	}
 
-    @AfterClass
-    void afterClass() {
-        System.out.println("After Class");
-    }
+	@AfterClass
+	void afterClass() {
+		System.out.println("After Class");
+	}
 
-    @AfterMethod
-    void afterMethod() {
-        System.out.println("After renamedMethod");
-        System.out.println("This is typed in the new branch");
-    }
+	@AfterMethod
+	void afterMethod() {
+		System.out.println("After renamedMethod");
+		System.out.println("This is typed in the new branch");
+	}
 
-    @BeforeMethod
-    void beforeMethod() {
-        System.out.println("Before renamedMethod");
-    }
+	@BeforeMethod
+	void beforeMethod() {
+		System.out.println("Before renamedMethod");
+	}
 
-    @Test
-    public void greenTestNGTest() {
-        System.out.println("testNGTest running");
-        Assert.assertEquals(1, 1);
-    }
-
-
-    @Test
-    public void yellowTestNGTestNegative() {
-        System.out.println("testNGTestNegative running");
-        Assert.assertEquals(1, 2);
-    }
-
-    @Test
-    public void redTestNGTest() {
-        System.out.println("testNGTestRed running");
-        int i = 1 / 0;
-        Assert.assertEquals(i, 1);
-    }
-
-    @Test(expectedExceptions = ArithmeticException.class)
-    public void greenTestNGTestExpectedException() {
-        System.out.println("testNGTestExpectedException running");
-        int i = 1 / 0;
-        Assert.assertEquals(1, i);
-
-    }
-
-    @Ignore
-    @Test
-    public void grayTestNGTestIgnored() {
-        System.out.println("testNGTestIgnored running");
-        System.out.println("" +
-                "pewpew");
-        int i = 1 / 0;
-        Assert.assertEquals(1, i);
-    }
-
-    @Test(successPercentage = 99, invocationCount = 5)
-    public void testNGWithPercentage10() {
-        for (int i = 0; i < 10; i++) {
-            Assert.assertTrue(i < 5);
-        }
-    }
+	@Test
+	public void greenTestNGTest() {
+		System.out.println("testNGTest running");
+		Assert.assertEquals(1, 1);
+	}
 
 
+	@Test
+	public void yellowTestNGTestNegative() {
+		System.out.println("testNGTestNegative running");
+		Assert.assertEquals(1, 2);
+	}
+
+	@Test
+	public void redTestNGTest() {
+		System.out.println("testNGTestRed running");
+		int i = 1 / 0;
+		Assert.assertEquals(i, 1);
+	}
+
+	@Test(expectedExceptions = ArithmeticException.class)
+	public void greenTestNGTestExpectedException() {
+		System.out.println("testNGTestExpectedException running");
+		int i = 1 / 0;
+		Assert.assertEquals(1, i);
+	}
+
+	@Ignore
+	@Test
+	public void grayTestNGTestIgnored() {
+		System.out.println("testNGTestIgnored running");
+		System.out.println("" +
+				"pewpew");
+		int i = 1 / 0;
+		Assert.assertEquals(1, i);
+	}
+
+	@Test(successPercentage = 99, invocationCount = 5)
+	public void testNGWithPercentage10() {
+		for (int i = 0; i < 10; i++) {
+			Assert.assertTrue(i < 5);
+		}
+	}
 }
